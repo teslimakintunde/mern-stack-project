25e600c016865c3f66eea4bb8b1680b1e4cc4202
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-04-17 11:28:40 +0100

    maintaining single gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../.vercel/output/static",
+    emptyOutDir: true,
+    sourcemap: true, // Recommended for debugging
+  },
+});
